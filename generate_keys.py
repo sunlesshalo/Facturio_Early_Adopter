51663f5726cb990e126dc60f876f1c4647463316
Ferencz Csuszner <39485506-ferencz1@users.noreply.replit.com>
2025-02-25 05:52:13 +0000

    Upgraded generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -3,9 +3,9 @@ import secrets
 
 # Generate a new master key (Fernet key is URL-safe base64-encoded 32 bytes)
 master_key = Fernet.generate_key()
-print(f"Master key: {master_key.decode()}")  # Save this key securely!
-print(f"Flask secret key: {Fernet.generate_key().decode()}")
+print(f"CONFIG_KEY: {master_key.decode()}")  # Save this key securely!
+print(f"FLASK_SECRET_KEY: {Fernet.generate_key().decode()}")
 
 
 csrf_token = secrets.token_urlsafe(64)
-print(f"CSRF token: {csrf_token}")
+print(f"WTF_CSRF_SECRET_KEY: {csrf_token}")
